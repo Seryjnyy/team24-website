96374c9093c92e02878a71b3b829867934f37e6c
Jakub Wojcik <psyjw27@exmail.nottingham.ac.uk>
2022-05-08 11:27:38 +0100

    Updated AI link to AWS API

--- src/component/Dashboard/CarbonIntensity/CarbonIntensityGraph.js
+++ src/component/Dashboard/CarbonIntensity/CarbonIntensityGraph.js
@@ -15,7 +15,7 @@ function EnergyGraph() {
 
   async function fetchAIData() {
     const dateString = date.toLocaleDateString('en-CA');
-    const res = await fetch("http://3.8.138.22:6001/requests/getCarbonData?fromDate=" + dateString + "&toDate=" + dateString);
+    const res = await fetch("https://e08ew9jcuh.execute-api.eu-west-2.amazonaws.com/AICarbonIntensity?fromDate=" + dateString + "&toDate=" + dateString);
     return res.json();
   }
 
